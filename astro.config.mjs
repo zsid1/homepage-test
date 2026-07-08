efed8881e53ecd63783a51b779817745c5f9d2c1
zsid1 <83797392+zsid1@users.noreply.github.com>
2026-07-08 00:17:26 -0500

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://www.pcadoc.com",
+  integrations: [sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
